fe4cc3ff851f5e597a6b7cb6775b7e880ce4fb11
Atalay Özkan <matalayath@gmail.com>
2021-10-31 04:46:43 +0300

    aggregate functions commands

---
+++ 2_AGGREGATE_FUNCTIONS.sql
@@ -0,0 +1,43 @@
+﻿-- @Author Atalay Özkan
+--Aggregate functions
+--Sum,Count,Min,Max,Avg
+
+SELECT BRANCH,count(*) as TrxNum ,sum(LINENET) as SumSales,min(LINENET) as MinimumPrice,max(LINENET) as MaximumPrice, AVG(LINENET) AS AvreagaSales FROM SALES --WHERE BRANCH='İstanbul Subesi'
+GROUP BY BRANCH
+
+
+SELECT TOP 10 * FROM SALES
+
+-- How do I retrive the data of the branch with the maximum sales?
+
+SELECT TOP 10 BRANCH AS BRANCH_NAME, SUM(LINENET) AS SUMSALES
+FROM SALES 
+GROUP BY BRANCH
+ORDER BY SUM(LINENET) DESC
+
+-- To be able to retrive the branch name and sales sum data which is bigger than 50k?
+select BRANCH,FORMAT(SUM(LINENET),'N') as SumSales from sales
+--where SUM(LINENET)> 50000
+group by BRANCH 
+having SUM(LINENET)>= 50000 and branch is not null
+order by SUM(LINENET) desc
+
+
+--To be able to retrive the branch of sales daily report
+select * from sales where branch LIKE 'BURSA%' and date_='20170105'
+
+
+select branch,date_, SUM(LINENET) as SumSales,count(*) as numRow
+from sales
+where branch='Bursa Subesi'
+Group by branch,date_
+order by branch, SumSales desc
+
+
+-- the branch of sales data according to date
+select date_,branch, sum(LINENET) as SumSales from sales
+where branch is not null
+group by date_,branch
+order by date_ 
+
+
